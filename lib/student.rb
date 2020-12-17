788c868d1da05903a91073791fe39f6ea5bbc5ef
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-12-17 00:42:54 -0600

    commit tree 0dbd503a3da2377c0a3645a70055a093153ddaf8

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require "./user.rb"
+class Student < User
+
+    def initialize
+        @first_name
+        @last_name
+        @knowledge = []
+    end
+
+    def learn(fact)
+        @knowledge << fact
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
